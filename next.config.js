66548578955afa4752f1e2b2851384ba182c216a
nitingupta95 <ng61315@gmail.com>
2025-08-27 12:06:22 +0530

    added all files

--- next.config.js
+++ next.config.js
@@ -5,6 +5,15 @@
 import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+    eslint: {
+        // We run ESLint in a separate process as part of our CI step
+        ignoreDuringBuilds: true,
+    },
+    typescript:{
+        // We run type checking in a separate process as part of our CI step
+        ignoreBuildErrors: true,
+    }
+};
 
 export default config;
